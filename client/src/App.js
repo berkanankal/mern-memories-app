6092b12508e2e670a2105996f74de090b5676450
brknkl <berkanankal68@gmail.com>
2022-03-08 16:42:23 +0300

    Redux toolkit implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addPost } from "./redux/postsSlice";
 
 const App = () => {
+  const { posts } = useSelector((state) => state.posts);
+
+  console.log(posts);
+
   return <div>Hello World!</div>;
 };
 
